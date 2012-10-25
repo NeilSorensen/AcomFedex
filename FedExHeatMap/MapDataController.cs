4743988372790c6860a61f5c24555ffe39ee601d
Craig Peterson <cpeterson@ancestry.com>
2012-10-25 13:28:57 -0600

    Map showing every 5 seconds.

--- FedExHeatMap/MapDataController.cs
+++ FedExHeatMap/MapDataController.cs
@@ -12,7 +12,7 @@ namespace FedExHeatMap
         {
             return new List<MapData>
                        {
-                           new MapData {Latitude = 37.42426708029149, Longitude = -122.0840722197085, Weight = 1},
+                           new MapData {Latitude = 37.42426708029149, Longitude = -122.0840722197085, Weight = 10},
                            new MapData {Latitude = 40.1748, Longitude = -111.4140, Weight = 10},
                            new MapData {Latitude = 20.1748, Longitude = -121.4140, Weight = 50},
                            new MapData {Latitude = -10.1748, Longitude = 11.4140, Weight = 100},
