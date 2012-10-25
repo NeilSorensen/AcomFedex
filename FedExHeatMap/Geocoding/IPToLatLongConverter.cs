a7dcefdc24d08f03f687d25be9b8bab9839b61a5
Craig Peterson <cpeterson@ancestry.com>
2012-10-25 14:09:38 -0600

    Ip Geocoder Works?

---
+++ FedExHeatMap/Geocoding/IPToLatLongConverter.cs
@@ -0,0 +1,13 @@
+﻿namespace FedExHeatMap.Geocoding
+{
+    public class IpToLatLongConverter
+    {
+        static LookupService service = new LookupService(@"D:\Craig\AcomFedex\FedExHeatMap\GeoLiteCity.dat", LookupService.GEOIP_STANDARD);
+
+        public static MapData ConvertToLatLong(string ip)
+        {
+            var loc = service.getLocation(ip);
+            return new MapData {Latitude = loc.latitude, Longitude = loc.longitude, Weight = 1};
+        }
+    }
+}
